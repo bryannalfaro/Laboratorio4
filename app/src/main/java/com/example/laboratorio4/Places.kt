9665b50da42f4a7f76496755006d5ac8fb75c8a7
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-02-06 20:45:30 -0600

    Primer Fragment añadido

--- app/src/main/java/com/example/laboratorio4/Places.kt
+++ app/src/main/java/com/example/laboratorio4/Places.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.laboratorio4.databinding.FragmentPlacesBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -17,7 +19,10 @@ class Places : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_places, container, false)
+        val binding = DataBindingUtil.inflate<FragmentPlacesBinding>(
+            inflater, R.layout.fragment_places, container, false)
+
+        return binding.root
     }
 
 
--- app/src/main/java/com/example/laboratorio4/Principal.kt
+++ app/src/main/java/com/example/laboratorio4/Principal.kt
@@ -1,24 +1,85 @@
 package com.example.laboratorio4
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.laboratorio4.databinding.FragmentPrincipalBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class Principal : Fragment() {
 
+    private var tester:Boolean?=true//Evaluate if the FloatingButton is selected
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_principal, container, false)
+
+        val binding1 = DataBindingUtil.inflate<FragmentPrincipalBinding>(
+            inflater, R.layout.fragment_principal, container, false)
+
+        binding1.star
+        binding1.star.setOnClickListener{
+            if(tester==true){
+                var name=binding1.editText2.text.toString()
+
+                binding1.nameSet.setText(name)
+                binding1.editText2.setText("")
+                binding1.editText2.visibility= View.GONE
+                binding1.textView3.visibility= View.GONE
+                binding1.nameSet.visibility= View.VISIBLE
+                tester=false
+            }else{
+                binding1.editText2.visibility= View.VISIBLE
+                binding1.textView3.visibility= View.VISIBLE
+                binding1.nameSet.visibility= View.GONE
+                tester=true
+            }
+        }
+        return binding1.root
+
+
+
+//        binding.tikalButton
+//        binding.tikalButton.setOnClickListener {
+//            val intent = Intent(this, Places::class.java)
+//            intent.putExtra("boton6","Tikal")
+//
+//            startActivity(intent)
+//
+//        }
+//        binding.antiguaButton
+//        binding.antiguaButton.setOnClickListener {
+//            val intent = Intent(this, Places::class.java)
+//            intent.putExtra("boton7","Antigua")
+//
+//            startActivity(intent)
+//
+//        }
+//        binding.semucButton
+//        binding.semucButton.setOnClickListener {
+//
+//            val intent = Intent(this, Places::class.java)
+//            intent.putExtra("boton8","Semuc")
+//
+//            startActivity(intent)
+//
+//        }
+
+
     }
+
+
 
 
 }
