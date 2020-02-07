41ce75cf8908c1470d985cfd10c631c84342c282
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-02-07 17:14:34 -0600

    share option completada

--- app/src/main/java/com/example/laboratorio4/Comentario.kt
+++ app/src/main/java/com/example/laboratorio4/Comentario.kt
@@ -1,11 +1,10 @@
 package com.example.laboratorio4
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.laboratorio4.databinding.FragmentComentarioBinding
@@ -25,9 +24,36 @@ class Comentario : Fragment() {
 
         val string: String? = arguments?.getString("comentario")
         binding3.comment.setText(string)
+        setHasOptionsMenu(true)
+
+
         // Inflate the layout for this fragment
         return binding3.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.share,menu)
+    }
 
+    //Cambiar luego
+    private fun getShareIntent():Intent{
+        val string1: String? = arguments?.getString("comentario")
+
+        val shareIntent=Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain").putExtra(Intent.EXTRA_TEXT,string1)
+
+        return shareIntent
+    }
+
+    private fun shareSuccess(){
+        startActivity(getShareIntent())
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item!!.itemId){
+            R.id.shareButton->shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
